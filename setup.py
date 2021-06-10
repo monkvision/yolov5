829ff89510c398ce8713a2988f7d04acdc76264a
Ubuntu <ubuntu@ip-172-31-8-194.eu-west-1.compute.internal>
2021-06-10 10:49:59 +0000

    do not find the train package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="yolov5",
     version="0.0.1",
-    packages=find_packages(),
+    packages=find_packages(exclude=("train",)),
     url="https://github.com/monkvision/monk",
     author="Ultralytics",
     install_requires=[
