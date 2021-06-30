250ec3e025716b693aa9c2c9fd76e425551e1e09
Mathis Chenuet <artdevelopp@hotmail.fr>
2021-06-30 13:35:44 +0000

    Fix package url to yolov5

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ setup(
     name="yolov5",
     version="0.0.1",
     packages=find_packages(exclude=("train",)),
-    url="https://github.com/monkvision/monk",
+    url="https://github.com/monkvision/yolov5",
     author="Ultralytics",
     install_requires=[
         "Cython>=0.29.14",
         "numpy>=1.18.5",
-        "opencv-python>=4.1.2", # "opencv_python_headless>=4.1.2.30",
+        "opencv-python>=4.1.2",  # "opencv_python_headless>=4.1.2.30",
         # "torch>=1.5.1",
         "easydict>=1.9",
         "matplotlib>=3.1.2",
@@ -18,6 +18,6 @@ setup(
         "PyYAML",
         # "torchvision>=0.4.2",
         "scipy>=1.3.3",
-        "tqdm>=4.40.0"
+        "tqdm>=4.40.0",
     ],
 )
